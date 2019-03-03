bcd802125abfec9b6fe723ae939dfd06c7410438
shun <shunichiroid@gmail.com>
2019-03-03 21:44:23 +0900

    Pulling Data From The Database

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -24,4 +24,9 @@ class MessagesController extends Controller
         // Redirect
         return redirect('/')->with('success', 'Message Sent');
     }
+
+    public function getMessages() {
+        $messages = Message::all();
+        return view('messages')->with('messages', $messages);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/', 'PagesController@getHome');
+Route::get('/about', 'PagesController@getAbout');
+Route::get('/contact', 'PagesController@getContact');
+Route::get('/messages', 'MessagesController@getMessages');
 
 Route::post('/contact/submit', 'MessagesController@submit');
